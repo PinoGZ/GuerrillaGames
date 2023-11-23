8d2c935b6199d7fcc5b089f33ef8d284fd183328
Brais López Escudero <braislopezescudero@gmail.com>
2023-11-23 15:39:17 +0100

    Fix

--- Index/games/tanque/main.js
+++ Index/games/tanque/main.js
@@ -1,259 +1,357 @@
-var gravity = 0.1;
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-$(document).ready(function(){
-	gameLoop();
-});
+var player_lives = 3;
+var score = 0;
 
-function fire(fireObj){
-	
-	
-	var fireItem = $(fireObj.selector);
-	var position = fireItem.position();
+var tank_width = 30;
+var tank_height = 40;
+var tank_speed = 0.55; //actually used to increment the Y coords of all the blocks, set at 0.3
 
-	var tempCounter = 7;
-	var upSpeed = 0;
-	var downSpeed = 0;
-	var distance = 5 + (fireObj.damage / 10);
-	var timer = setInterval(function(){
-		if($("#player2").length > 0 && collision(fireItem, $("#player2"))){
-			$("#player2").remove();
-			clearInterval(timer);
-			BOOOOOM(fireObj);
-			respawnPlayer("player2");
-			return;
-		}
-		if($("#player1").length > 0 && collision(fireItem, $("#player1"))){
-			$("#player1").remove();
-			clearInterval(timer);
-			BOOOOOM(fireObj);
-			respawnPlayer("player1");
-			return;
-		}
-		position = fireItem.position();
-		
-		if(distance > 0){
-			upSpeed = (fireObj.speed * distance) / 30;
-			fireItem.css("top", (position.top - upSpeed) + "px");
-			distance --;
-		}
-		else{
-			if(tempCounter > 0){
-				tempCounter--;
-			}
-			else{
-				if(!collision(fireItem, $(".ground"))){
-					downSpeed += upSpeed + gravity;
-					fireItem.css("top", (position.top + downSpeed) + "px");
-				}
-				else{
-					clearInterval(timer);
-					BOOOOOM(fireObj);
-				}
-			}
-		}
-		
-		if(fireObj.direction == "l"){
-			if(position.left > 0){
-				fireItem.css("left", (position.left - fireObj.speed) + "px");
-			}
-			else{
-				clearInterval(timer);
-				BOOOOOM(fireObj);
-			}
-		}
-		else if(fireObj.direction == "r"){
-			if(position.left < 780){
-				fireItem.css("left", (position.left + fireObj.speed) + "px");
-			}
-			else{
-				clearInterval(timer);
-				BOOOOOM(fireObj);
-			}
-		}
-	},30);
-	
+var tank = new Map();
+tank.set("X", canvas.width / 2);
+tank.set("Y", canvas.height - 70);
+tank.set("width", tank_width);
+tank.set("height", tank_height);
+
+var balls = [];
+var ball_speed = 1.6;
+var since_last_fire = performance.now();
+
+var blocks = [];
+var tank_block_collision_bool = true;
+
+var monsters = [];
+var monster_speed = 0.6; //speed at which the monsters move along the y axis.
+
+//This part handles the pressing of keys used to move the tank and fire
+var right_pressed = false;
+var left_pressed = false;
+var space_pressed = false;
+
+document.addEventListener("keydown", KeyDownFunc, false);
+document.addEventListener("keyup", KeyUpFunc, false);
+
+function KeyDownFunc(e) {
+  if (e.keyCode == 39) {
+    right_pressed = true;
+  } else if (e.keyCode == 37) {
+    left_pressed = true;
+  } else if (e.keyCode == 32) {
+    space_pressed = true;
+  }
 }
 
-function BOOOOOM(fireObj){
-	var fireItem = $(fireObj.selector);
-	var timer = setInterval(function(){
-		var width = fireItem.width();
-		var height = fireItem.height()
-		fireItem.width(width + 8);
-		fireItem.height(height + 8);
-		if(width > fireObj.radius ){
-			clearInterval(timer);
-			setTimeout(function(){
-				fireItem.remove();
-			},500)
-		}
-
-	},1);
-	
+function KeyUpFunc(e) {
+  if (e.keyCode == 39) {
+    right_pressed = false;
+  } else if (e.keyCode == 37) {
+    left_pressed = false;
+  } else if (e.keyCode == 32) {
+    space_pressed = false;
+  }
 }
 
-var fireID = 0;
-var p1Direction = "r";
-var p1Power = 10;
-var p2Direction = "r";
-var p2Power = 10;
-function gameLoop() {
-	
-	var moveSpeed = 10;
+//Drawing the Tank
+function drawTank() {
+  ctx.beginPath();
+  ctx.rect(tank.get("X"), tank.get("Y"), tank_width, tank_height);
+  ctx.fillStyle = "green";
 
-	if (keyPressed["65"]) {
-		p1Direction = "l";
-		move("#player1" , ((moveSpeed) - moveSpeed * 2) , 0);
-    } 
-	if (keyPressed["68"]) {
-		p1Direction = "r";
-		move("#player1" , moveSpeed , 0);
+  ctx.rect(tank.get("X") + tank_width / 2 - 5, tank.get("Y") - 15, 10, 15);
+  ctx.fillStyle = "green";
+
+  ctx.fill();
+  ctx.closePath();
+}
+
+//Drawing the border blocks
+function drawBorder() {
+  ctx.beginPath();
+  ctx.rect(0, 0, 80, canvas.height);
+  ctx.rect(canvas.width - 80, 0, 80, canvas.height);
+  ctx.fillStyle = "grey";
+  ctx.fill();
+  ctx.closePath();
+}
+
+//Initializing a new ball (starting position), adding it to a list
+function drawNewBall(ball_X, ball_Y) {
+  ctx.beginPath();
+  ctx.arc(ball_X, ball_Y, 5, 0, Math.PI * 2);
+
+  var ball = new Map();
+  ball.set("X", ball_X);
+  ball.set("Y", ball_Y);
+  ball.set("width", 3);
+  ball.set("height", 3);
+  balls.push(ball);
+  since_last_fire = performance.now();
+}
+
+//drawing all of the balls of the list
+function drawBalls() {
+  for (var i = 0; i < balls.length; i++) {
+    ctx.beginPath();
+    ctx.arc(balls[i].get("X"), balls[i].get("Y"), 5, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+//Generates coordinates with 80 < x < (canvas.width - 120) and -260 < y < -60. Returns the coordinates.
+function generateCoords() {
+  do {
+    var X = Math.random() * (canvas.width - 80) + 80;
+  } while (X + 120 > canvas.width);
+
+  var Y = Math.random() * (-260 - 60) - 60;
+  return [X, Y];
+}
+
+//Checks that the distance between TWO blocks is greater than 140 and the difference along the Y-axis is greater than 40
+function distanceCheck(X1, Y1, X2, Y2) {
+  var distance = Math.sqrt(Math.pow(X1 - X2, 2) + Math.pow(Y1 - Y2, 2));
+  if (distance > 140 && Math.abs(Y1 - Y2) > 40) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//function that returns TRUE if we need to generate new coords because the blocks are too close, FALSE otherwise
+function blockDistanceChecker(X, Y) {
+  if (blocks.length == 0) {
+    return false;
+  }
+
+  var check = false;
+  for (i = 0; i < blocks.length; i++) {
+    if (distanceCheck(X, Y, blocks[i].get("X"), blocks[i].get("Y"))) {
+      check = check || false;
+    } else {
+      check = check || true;
     }
-	
-	if(keyPressed["81"]){
-		
-		if(p1Power < 100){
-			p1Power += 3;
-			$("#player1 .power").css("width", (p1Power) + "%");
-		}
-		
-	}
-	
-	if (keyPressed["37"]) {
-		p2Direction = "l";
-		move("#player2" , ((moveSpeed) - moveSpeed * 2) , 0);
-    } 
-	if (keyPressed["39"]) {
-		p2Direction = "r";
-		move("#player2" , moveSpeed , 0);
+  }
+
+  if (!check) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//Creates a new block
+function drawNewBlock() {
+  do {
+    var coords = generateCoords();
+    var X = coords[0];
+    var Y = coords[1];
+  } while (blockDistanceChecker(X, Y));
+
+  var width = 40;
+  var height = 60;
+
+  var block = new Map();
+  block.set("X", X);
+  block.set("Y", Y);
+  block.set("width", width);
+  block.set("height", height);
+
+  blocks.push(block);
+}
+
+function drawBlocks() {
+  for (var i = 0; i < blocks.length; i++) {
+    ctx.beginPath();
+    ctx.rect(blocks[i].get("X"), blocks[i].get("Y"), blocks[i].get("width"), blocks[i].get("height"));
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+//Mover function: moves blocks elements down (y++) along the y axis
+function moverFunc() {
+  for (var i = 0; i < blocks.length; i++) {
+    blocks[i].set("Y", blocks[i].get("Y") + tank_speed);
+    //Drops the block from the blocks array when they're out of view
+    if (blocks[i].get("Y") > canvas.width) {
+      blocks.splice(i, 1);
     }
-	
-	if(keyPressed["75"]){
-		
-		if(p2Power < 100){
-			p2Power += 3;
-			$("#player2 .power").css("width", (p2Power) + "%");
-		}
-		
-	}
-
-  
-	
-	setTimeout(gameLoop, 24);
- };
- 
-function move(item,x,y){
-	 position = $(item).position();
-	 $(item).css("left", (position.left + x) + "px")
-	 $(item).css("top", (position.top + y) + "px")
- }
- 
- 
- 
-// General functions -----------------------------------------------------
-function collision($div1, $div2) {
-	var x1 = $div1.offset().left;
-	var y1 = $div1.offset().top;
-	var h1 = $div1.outerHeight(true);
-	var w1 = $div1.outerWidth(true);
-	var b1 = y1 + h1;
-	var r1 = x1 + w1;
-	var x2 = $div2.offset().left;
-	var y2 = $div2.offset().top;
-	var h2 = $div2.outerHeight(true);
-	var w2 = $div2.outerWidth(true);
-	var b2 = y2 + h2;
-	var r2 = x2 + w2;
-
-	if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-	return true;
+  }
 }
 
-function timeToMMSS(time){
-	var min = 0
-	var sec = time;
-	
-	if(sec > 59){
-		min = Math.floor(sec / 60);
-		sec = Math.floor(sec % 60);
-	}
-	
-	if(min < 10)
-		min = "0" + min;
-	
-	if(sec < 10)
-		sec = "0" + sec;
-	
-	
-	return min + ":" + sec;
+//Mover function: moves the balls the balls up (y--) along the y axis //ADDED: Moving the monsters
+function moveBalls() {
+  //Moving the Balls
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].set("Y", balls[i].get("Y") - ball_speed);
+    //Drops the ball from the balls array when they're out of view
+    if (balls[i].get("Y") < 0) {
+      balls.splice(i, 1);
+    }
+  }
+
+  //Moving the Monsters
+  for (var j = 0; j < monsters.length; j++) {
+    monsters[j].set("Y", monsters[j].get("Y") + monster_speed);
+    //Drops the monsters from the list when they're out of view
+    if (monsters[j].get("Y") > canvas.width) {
+      monsters.splice(j, 1);
+    }
+  }
 }
 
- function getRandomColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	for (var i = 0; i < 6; i++ ) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
+//Block Collision detection function
+function tank_block_collision() {
+
+  for (i = 0; i < blocks.length; i++) {
+    var conflict_X = false;
+    var conflict_Y = false;
+
+    if (tank.get("X") + tank_width > blocks[i].get("X") && tank.get("X") < blocks[i].get("X") + 40) {
+      conflict_X = conflict_X || true;
+    }
+    if (tank.get("Y") < blocks[i].get("Y") + 60 && tank.get("Y") > blocks[i].get("Y")) {
+      conflict_Y = conflict_Y || true;
+    }
+    if (conflict_X && conflict_Y) {
+      tank_block_collision_bool = false;
+      player_lives -= 1;
+      return;
+    }
+  }
+  tank_block_collision_bool = true;
 }
 
+//Generates X and Y coordinates for a new monster
+function create_monster() {
+  var coords = generateCoords();
+  var X = coords[0];
+  var Y = coords[1];
 
-var keyPressed = {};
-
-document.addEventListener('keydown', function(e) {
-   keyPressed[e.keyCode] = true;
-}, false);
-document.addEventListener('keyup', function(e) {
-
-	if(e.keyCode == 81){
-		var speed = p1Power / 10;
-		if(speed < 5){
-			speed = 5;
-		}
-		fireID++;;
-		var position = $("#player1").position();
-		console.log(position);
-		$("#board").append('<span id="fire' + fireID + '"class="fire" style="background:' + getRandomColor() + '; left:'+ (position.left + 7) +'px; top:' + (position.top - 20) + 'px;"></span>');
-		$("#fire" + fireID);
-
-		var fireObj = new Object();
-		fireObj.selector = "#fire" + fireID;
-		fireObj.direction = p1Direction;
-		fireObj.damage = p1Power; 
-		fireObj.radius = p1Power / 3;
-		fireObj.speed = speed;
-		fire(fireObj);
-		$("#player1 .power").css("width", "0%");
-		p1Power = 10;
-	}
-	
-	if(e.keyCode == 75){
-		var speed = p2Power / 10;
-		if(speed < 5){
-			speed = 5;
-		}
-		fireID++;;
-		var position = $("#player2").position();
-		$("#board").append('<span id="fire' + fireID + '"class="fire" style="background:' + getRandomColor() + '; left:'+ (position.left + 7) +'px; top:' + (position.top - 20) + 'px;"></span>');
-		$("#fire" + fireID);
-
-		var fireObj = new Object();
-		fireObj.selector = "#fire" + fireID;
-		fireObj.direction = p2Direction;
-		fireObj.damage = p2Power; 
-		fireObj.radius = p2Power / 3;
-		fireObj.speed = speed;
-		fire(fireObj);
-		$("#player2 .power").css("width", "0%");
-		p2Power = 10;
-	}
-   keyPressed[e.keyCode] = false;
-}, false);
-
-function respawnPlayer(player){
-	setTimeout(function(){
-		$("#board").append('<div id="' + player + '" class="player"><span class="power"></span></div>');
-	}, 3000);
-	
+  var monster = new Map();
+  monster.set("X", X);
+  monster.set("Y", Y);
+  monster.set("width", 25);
+  monster.set("height", 29);
+  monsters.push(monster);
 }
+
+//Draws a monster
+function draw_monster(X, Y) {
+  var scale = 0.8;
+  var h = 9; //height
+  var a = 5;
+
+  ctx.beginPath();
+  //First trapezoid
+  ctx.moveTo(X, Y);
+  ctx.lineTo(X - a * scale, Y + h * scale);
+  ctx.lineTo(X + (a * 4) * scale, Y + h * scale);
+  ctx.lineTo(X + (a * 3) * scale, Y);
+  //Second trapezoid
+  ctx.moveTo(X - (a + 5) * scale, Y + h * scale);
+  ctx.lineTo(X - (a) * scale, Y + (h + 20) * scale);
+  ctx.lineTo(X + (a + 15) * scale, Y + (h + 20) * scale);
+  ctx.lineTo(X + (a + 20) * scale, Y + (h) * scale);
+
+  ctx.fillStyle = "purple";
+  ctx.fill();
+  ctx.closePath();
+}
+
+//Draws all monsters in the monsters list
+function draw_monsters() {
+  for (i = 0; i < monsters.length; i++) {
+    var X = monsters[i].get("X");
+    var Y = monsters[i].get("Y");
+    draw_monster(X, Y);
+  }
+}
+
+//Collision detector: detects a collision along the Y-axis between two objects (Maps) with the following template: ["X", "Y", "width", "height"]
+function collision_detector(first, second) {
+  var x1 = first.get("X");
+  var y1 = first.get("Y");
+  var width1 = first.get("width");
+  var height1 = first.get("height");
+  var x2 = second.get("X");
+  var y2 = second.get("Y");
+  var width2 = second.get("width");
+  var height2 = second.get("height");
+
+  if (x2 > x1 && x2 < x1 + width1 || x1 > x2 && x1 < x2 + width2) {
+    if (y2 > y1 && y2 < y1 + height1 || y1 > y2 && y1 < y2 + height2) {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+//detects a collion betweenn balls and monsters
+function ball_monster_collision() {
+  for (var i = 0; i < monsters.length; i++) {
+    var monster = monsters[i];
+    for (var j = 0; j < balls.length; j++) {
+      var ball = balls[j];
+      if (collision_detector(monster, ball)) {
+        balls.splice(j, 1);
+        monsters.splice(i, 1);
+        score += 2;
+      }
+    }
+  }
+}
+
+//Used to display the number of lives remaining and game score
+function drawInfo() {
+  ctx.font = "bold 15px Gill Sans MT";
+  ctx.fillStyle = "blue";
+  ctx.fillText("Lives: " + player_lives, 635, 22);
+  ctx.fillText("Score: " + score, 13, 22)
+}
+
+//Main function, will be used to run the game
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); 
+  drawBorder();
+  drawInfo();
+  drawTank();
+  drawBalls();
+  drawBlocks();
+  draw_monsters();
+  moveBalls();
+  tank_block_collision();
+  ball_monster_collision();
+  moverFunc();
+
+  if (space_pressed && balls.length < 10 && performance.now() - since_last_fire > 500) {
+    drawNewBall(tank.get("X") + 15, tank.get("Y") - 30);
+  }
+  if (right_pressed && tank.get("X") + tank_width < canvas.width) {
+    tank.set("X", tank.get("X") + 1);
+  }
+  if (left_pressed && tank.get("X") > 0) {
+    tank.set("X", tank.get("X") - 1);
+  }
+
+  if (blocks.length < 3) {
+    drawNewBlock();
+  }
+  if (monsters.length < 1) {
+    create_monster();
+  }
+  if (!tank_block_collision_bool && player_lives < 0) {
+     alert("You lost");
+     document.location.reload()
+  }
+
+  requestAnimationFrame(draw);
+}
+
+draw();
 
--- Index/js/main.js
+++ Index/js/main.js
@@ -1,125 +1,42 @@
-var bg = document.querySelector('.item-bg');
-var items = document.querySelectorAll('.game__item');
-var item = document.querySelector('.game__item');
+var bg = document.querySelector(".item-bg");
+var items = document.querySelectorAll(".game__item");
 
 function cLog(content) {
-    console.log(content)
+  console.log(content);
 }
 
-if($(window).width() > 800) {
-    $(document).on("mouseover", ".game__item", function (_event, _element) {
+if (window.innerWidth > 800) {
+  items.forEach(function (element, index) {
+    element.addEventListener("mouseover", function () {
+      var x = this.getBoundingClientRect().left;
+      var y = this.getBoundingClientRect().top;
+      var width = this.getBoundingClientRect().width;
+      var height = this.getBoundingClientRect().height;
 
-        var newsItem = document.querySelectorAll('.game__item');
-        newsItem.forEach(function (element, index) {
-            element.addEventListener('mouseover', function () {
-                var x = this.getBoundingClientRect().left;
-                var y = this.getBoundingClientRect().top;
-                var width = this.getBoundingClientRect().width;
-                var height = this.getBoundingClientRect().height;
+      // Remueve la clase "active" de todos los elementos con la clase "game__item"
+      items.forEach(function (item) {
+        item.classList.remove("active");
+      });
 
-                $('.item-bg').addClass('active');
-                $('.game__item').removeClass('active');
-               
+      // Añade la clase "active" al elemento actual
+      this.classList.add("active");
 
+      // Añade la clase "active" al elemento con la clase "item-bg"
+      bg.classList.add("active");
 
-                bg.style.width = width + 'px';
-                bg.style.height = height + 'px';
-                bg.style.transform = 'translateX(' + x + 'px ) translateY(' + y + 'px)';
-            });
-
-            element.addEventListener('mouseleave', function () {
-                $('.item-bg').removeClass('active');
-                $('.game__item').removeClass('active');
-            });
-
-        });
-
+      bg.style.width = width + "px";
+      bg.style.height = height + "px";
+      bg.style.transform = "translateX(" + x + "px ) translateY(" + y + "px)";
     });
+
+    element.addEventListener("mouseleave", function () {
+      // Remueve la clase "active" de todos los elementos con la clase "game__item"
+      items.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      // Remueve la clase "active" del elemento con la clase "item-bg"
+      bg.classList.remove("active");
+    });
+  });
 }
-
-
-var swiper = new Swiper('.game-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    keyboard: true,
-    spaceBetween: 0,
-    slidesPerView: 'auto',
-    speed: 300,
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 0,
-        modifier: 3,
-        slideShadows: false
-    },
-    breakpoints: {
-        480: {
-            spaceBetween: 0,
-            centeredSlides: true
-        }
-    },
-    simulateTouch: true,
-    navigation: {
-        nextEl: '.game-slider-next',
-        prevEl: '.game-slider-prev'
-    },
-    pagination: {
-        el: '.game-slider__pagination',
-        clickable: true
-    },
-    on: {
-        init: function () {
-            var activeItem = document.querySelector('.swiper-slide-active');
-
-            var sliderItem = activeItem.querySelector('.game__item');
-
-            $('.swiper-slide-active .game__item').addClass('active');
-
-            var x = sliderItem.getBoundingClientRect().left;
-            var y = sliderItem.getBoundingClientRect().top;
-            var width = sliderItem.getBoundingClientRect().width;
-            var height = sliderItem.getBoundingClientRect().height;
-
-
-            $('.item-bg').addClass('active');
-
-            bg.style.width = width + 'px';
-            bg.style.height = height + 'px';
-            bg.style.transform = 'translateX(' + x + 'px ) translateY(' + y + 'px)';
-        }
-    }
-});
-
-swiper.on('touchEnd', function () {
-    $('.game__item').removeClass('active');
-    $('.swiper-slide-active .game__item').addClass('active');
-});
-
-swiper.on('slideChange', function () {
-    $('.game__item').removeClass('active');
-});
-
-swiper.on('slideChangeTransitionEnd', function () {
-    $('.game__item').removeClass('active');
-    var activeItem = document.querySelector('.swiper-slide-active');
-
-    var sliderItem = activeItem.querySelector('.game__item');
-
-    $('.swiper-slide-active .game__item').addClass('active');
-
-    var x = sliderItem.getBoundingClientRect().left;
-    var y = sliderItem.getBoundingClientRect().top;
-    var width = sliderItem.getBoundingClientRect().width;
-    var height = sliderItem.getBoundingClientRect().height;
-
-
-    $('.item-bg').addClass('active');
-
-    bg.style.width = width + 'px';
-    bg.style.height = height + 'px';
-    bg.style.transform = 'translateX(' + x + 'px ) translateY(' + y + 'px)';
-});
-
-
